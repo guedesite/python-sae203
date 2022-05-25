4b8cbca81ec3b2759edac6a7745f3d3b4b317a33
hugom <hugom@client.client.org>
2022-05-25 17:22:56 +0200

    etape 3

--- aggreg.py
+++ aggreg.py
@@ -6,6 +6,10 @@ Created on Mon May 16 12:35:44 2022
 """
 import sys;
 import feedparser;
+import time;
+import yaml;
+from datetime import datetime;
+
 
 def charge_urls(liste_url):
     Return = [];
@@ -25,27 +29,68 @@ def fusion_flux(liste_url, liste_flux):
             for feed in liste_flux[i]:
                 dic = {};
                 dic["titre"] = feed["title"];
-                dic["categorie"] = feed["tags"][0]["term"];
+                dic["category"] = feed["tags"][0]["term"];
                 dic["serveur"] = liste_url[i];
                 dic["date_publi"] = feed["published"];
                 dic["lien"] = feed["link"];
                 dic["description"] = feed["summary"];
-                Return.append(dic);
+
+                element = datetime.strptime(dic["date_publi"],"%a, %d %b %Y %H:%M");
+                dic["time"] = int(datetime.timestamp(element));
+
+                if(len(Return) > 0):
+                    for i2 in range(len(Return)):
+                        if Return[i2]["time"] < dic["time"]:
+                            Return.insert(i2, dic);
+                            break;
+                else:
+                    Return.append(dic);
     return Return;
 
 def genere_html(liste_evenements, chemin_html):
+    output = "";
+    with open('assets/model.html', "r") as f:
+        content = f.read();
+
+        parts = content.split("{boucle}");
+
+        #Partie 1: header
+        actualTime = time.strftime("%a, %d %b %Y %H:%M", time.localtime());
+        output = parts[0].replace("{date-et-heure-actuelle}",actualTime);
+
+        #Partie 2:articles
+        for event in liste_evenements:
+            pre_output = parts[1];
+            for key in event:
+                pre_output = pre_output.replace("{"+str(key)+"}", str(event[key]));
+            output += pre_output;
+
+    with open(chemin_html, "w+") as f:
+        f.write(output);
+
+def load_config():
+    with open("config.yaml", "r") as configs:
+        return yaml.safe_load(configs);
+
+def generate_rssurls(urls, name):
+    Return = [];
+    for url in urls:
+        Return.append(url+"/"+name);
+    return Return;
 
 
 def main():
-    assert len(sys.argv) > 1;
 
-    rss_urls = sys.argv;
-    rss_urls.pop(0); # On supprime le fichier python de la liste des arguments
+    configs = load_config();
 
-    liste_flux = charge_urls(rss_urls);
 
-    liste_evenements = fusion_flux(rss_urls, liste_flux);
+    urls_rss = generate_rssurls(configs["sources"], configs["rss-name"]);
 
+    liste_flux = charge_urls(urls_rss);
+
+    liste_evenements = fusion_flux(configs["sources"], liste_flux);
+
+    genere_html(liste_evenements, configs["destination"]);
 
 if __name__ == "__main__":
     main();
